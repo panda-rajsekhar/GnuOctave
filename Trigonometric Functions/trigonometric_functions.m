afb013b6676c4afeb68f13c9837eab05e9c1b3d8
Rajsekhar Panda <132293033+panda-rajsekhar@users.noreply.github.com>
2025-08-01 17:56:51 +0530

    Update trigonometric_functions.m

--- Trigonometric Functions/trigonometric_functions.m
+++ Trigonometric Functions/trigonometric_functions.m
@@ -4,10 +4,10 @@ interval of 0 to 2pi and display them using the subplot function in the layout h
 %}
 
 x = 0:pi/100:2*pi ;
-% the variable x is defined as a vector from 0 to 2pi in  increment of pi/100
-% this gives a smooth curve when plotting the functions
+%the variable x is defined as a vector from 0 to 2pi in  increment of pi/100
+%this gives a smooth curve when plotting the functions
 
-% 2D subplot of sin x
+%2D subplot of sin x
 y = sin(x);
 subplot(3,2,1);
 plot(x,y)
@@ -28,39 +28,39 @@ then the hold function allows us to plot more on the same axes later ;
 we are going to follow the same process to plot the remainig functions ;
 %}
 
-% 2D plot of cos x
+%2D plot of cos x
 z = cos(x);
 subplot(3,2,2);
 plot(x,z)
 legend('cos(x)');
 hold
 
-% 2d plot of tan x
+%2d plot of tan x
 a = tan(x);
 subplot(3,2,3);
 plot(x,a)
 legend('tan(x)');
 hold
 
-% 2D plot of cosec x
+%2D plot of cosec x
 b = csc(x);
 subplot(3,2,4);
 plot(x,b)
 legend('csc(x)');
 hold
 
-% 2D plot of sec x
+%2D plot of sec x
 c = sec(x);
 subplot(3,2,5);
 plot(x,c)
 legend('sec(x)');
 hold
 
-% 2D plot of cot x
+%2D plot of cot x
 d = cot(x);
 subplot(3,2,6);
 plot(x,d)
 legend('cot(x)');
 
-% issues in  or  some questions that may arise in your minid are dealt in the readme file
+%issues in  or  some questions that may arise in your minid are dealt in the readme file
 
